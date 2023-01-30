951a81c187960d4878b62592194a8f34f33e1a28
Samuel <samuelsilveira100@gmail.com>
2023-01-30 03:49:40 -0300

    Adição de funções do curso. Conclusão do curso.

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento inexistente ou não aplicável para a função');
+    }
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.keyCode === 32 || evento.keyCode === 13) {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
