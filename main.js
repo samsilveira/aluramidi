f6c9158d186490b64b3e09c47d04a2c1c37b9b30
Samuel <samuelsilveira100@gmail.com>
2023-01-29 23:13:41 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert('Olá, mundo!');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
